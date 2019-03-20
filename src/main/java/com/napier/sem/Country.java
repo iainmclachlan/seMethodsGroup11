865ad4583453cbd927469090985f9159400c1a9c
iainmclachlan <iainmcl1990@gmail.com>
2019-03-20 11:15:11 +0000

    Added Country.java

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,52 @@
+package com.napier.sem;
+
+public class Country
+{
+    //Code Assigned to Country
+    public String country_code;
+
+    //Name of Country
+    public String country_name;
+
+    //Continent country is on
+    public  String country_continent;
+
+    //Region Country resides
+    public  String country_region;
+
+    //Surface Area
+    public double country_surfaceArea;
+
+    //Year of Independace
+    public int country_indyYear;
+
+    //Population
+    public int country_population;
+
+    //Average Life Expectancy
+    public int country_lifeExpectancy;
+
+    //GNP
+    public double country_GNP;
+
+    //GNPOLD
+    public  double country_GNPOLD;
+
+    //Local Name of Country
+    public String country_localName;
+    
+    //Form of Goverment
+    public String country_goverment;
+
+    //Current Head of State
+    public String country_headOfState;
+
+    //Capital city
+    public String country_capital;
+
+    //Country Code 2
+    public String country_code2;
+
+
+
+}
